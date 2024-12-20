74584a21bb69680e6ef8cce1dbdee3a1955b70f9
Dan Whitman <daniel.whitman.2@us.af.mil>
2024-12-20 16:35:43 -0500

    Removes RTC mode features now that only one RTC monotonic is provided, i.e. the best one.

--- shared/src/monotonic.rs
+++ shared/src/monotonic.rs
@@ -12,9 +12,6 @@ use crate::FONT;
 #[cfg(not(any(feature = "clock1k", feature = "clock32k")))]
 compile_error!("A clock feature must be specified");
 
-#[cfg(not(any(feature = "systick", feature = "mode0", feature = "mode1")))]
-compile_error!("Monotonic mode feature must be specified");
-
 #[cfg(feature = "clock1k")]
 type _ClockRate = rtc_clock::Clock1k;
 #[cfg(feature = "clock32k")]
@@ -22,10 +19,8 @@ type _ClockRate = rtc_clock::Clock32k;
 
 #[cfg(feature = "systick")]
 rtic_monotonics::systick_monotonic!(Mono, 200);
-#[cfg(feature = "mode0")]
-rtc_mode0_monotonic!(Mono, _ClockRate);
-#[cfg(feature = "mode1")]
-rtc_mode1_monotonic!(Mono, _ClockRate);
+#[cfg(not(feature = "systick"))]
+rtc_monotonic!(Mono, _ClockRate);
 
 #[cfg(any(feature = "systick"))]
 impl Mono {
@@ -41,7 +36,7 @@ impl Mono {
     }
 }
 
-#[cfg(any(feature = "mode0", feature = "mode1"))]
+#[cfg(not(any(feature = "systick")))]
 impl Mono {
     #[inline]
     pub fn delay_ms(delay: u32) -> impl Future<Output = ()> {
@@ -59,7 +54,7 @@ impl Mono {
     pub fn general_start(_syst: pac::SYST, _rtc: pac::Rtc) {
         #[cfg(feature = "systick")]
         Mono::start(_syst, 120_000_000);
-        #[cfg(any(feature = "mode0", feature = "mode1"))]
+        #[cfg(not(feature = "systick"))]
         Mono::start(_rtc);
     }
 }
@@ -78,10 +73,8 @@ pub fn display_monotonic_info(display: &mut DisplayDriver) {
 
     #[cfg(feature = "systick")]
     write!(writer, "systick  ").unwrap();
-    #[cfg(feature = "mode0")]
+    #[cfg(not(feature = "systick"))]
     write!(writer, "mode0 ").unwrap();
-    #[cfg(feature = "mode1")]
-    write!(writer, "mode1 ").unwrap();
 
     #[cfg(feature = "clock1k")]
     write!(writer, " 1k").unwrap();
